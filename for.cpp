cc025447bddb6039b21e645cf6087f20faf1be95
Fajla-Rabby <fajla.rabby008@gmail.com>
2023-03-04 21:21:40 +0600

    for loop

---
+++ for.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the value of n: ";
+    // cin >> n;
+
+    cout << "printing count from 1 to n: " << endl;
+
+    /*  for (int i = 0; i <= n; i++)
+     {
+         cout << i << endl;
+     }
+  */
+    /* int i = 1;
+    for(; ;)
+    {
+        if(i<=n)
+        {
+            cout<<i<<endl;
+        }
+        else
+        {
+            break;
+        }
+        i++;
+    } */
+
+    for (int a = 0, b = 1, c = 2; a >= 0 && b >= 1 && c >= 2; a--, b--, c--)
+    {
+        cout << a << " " << b << " " << c << endl;
+    }
+}
