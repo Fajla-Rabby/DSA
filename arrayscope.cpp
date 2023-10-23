1cb3d981749a922fb24cc0524b51a4e894c66472
Fajla-Rabby <fajla.rabby008@gmail.com>
2023-10-23 08:41:58 +0600

    linera search

---
+++ arrayscope.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void update(int arr[], int n)
+{
+    cout << "inside the function" << endl;
+
+    //updating the arrays first element
+    arr[0] = 120;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "going back to main function"<<endl;
+}
+
+int main()
+{
+    int arr[3] = {1, 2, 3};
+    update(arr, 3);
+
+    // Printing the array
+    for (int i = 0; i < 3; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
