beade619b5a19c328c89ea4e4d5e20cec1507cba
Fajla-Rabby <fajla.rabby008@gmail.com>
2023-10-30 01:29:19 +0600

    swap alternate

---
+++ palindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    // if a number is palindrome or not
+    
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -2,17 +2,33 @@
 
 using namespace std;
 
-int ap(int n)
-{
-    int ans = (3 * n + 7);
-    return ans;
-}
+// int ap(int n)
+// {
+//     int ans = (3 * n + 7);
+//     return ans;
+// }
 
 int main()
 {
-    int n;
-    cin>>n;
-    int result = ap(n);
-    cout<<result<<endl;
+    // print from 1 to 100
+    for (int i = 1; i <= 100; i++)
+        cout << i << endl;
 
+    // print 10 to 1
+    for (int i = 10; i >= 1; i--)
+        cout << i << endl;
+
+    // print odd number from 1 to 10
+    for (int i = 1; i <= 10; i += 2)
+        cout << i << endl;
+
+    // counts how many 2 divides 100
+    int x = 100;
+    int cnt = 0;
+    while(x % 2 == 0)
+    {
+        x = x / 2;
+        cnt++;
+    }
+    cout<<cnt<<endl;
 }
