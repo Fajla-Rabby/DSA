43c4199463d9c8494630b0a79b1e473ae5da3d7b
Fajla-Rabby <fajla.rabby008@gmail.com>
2024-06-04 23:01:53 +0600

    more patterns

--- pattern3.cpp
+++ pattern3.cpp
@@ -6,17 +6,20 @@ int main()
 {
     int n;
     cin>>n;
+    int count = 1;
     int i = 1;
-    while(i <= n)
+
+    while (i <= n)
     {
         int j = 1;
         while(j <= n)
         {
-            cout<<n-j+1;
-            j++;
+            cout<<count<<" ";
+            count = count + 1;
+            j = j + 1;
         }
         cout<<endl;
-        i++;
+        i = i + 1;
     }
     
 }
