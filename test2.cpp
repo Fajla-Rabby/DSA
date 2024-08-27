da69fccb1a94c05c0562645d99c99c789423ada9
Fajla-Rabby <fajla.rabby008@gmail.com>
2024-08-28 00:08:30 +0600

    string

--- test2.cpp
+++ test2.cpp
@@ -4,9 +4,28 @@ using namespace std;
 
 int main()
 {
-   string S;
-   getline(cin, S, '\\');
+   /* Input
+The first line contains a string A (1 ≤ |A| ≤ 10) where |A| is the length of A.
 
-   cout<< S<< endl;
-   return 0; 
+The second line contains a string B (1 ≤ |B| ≤ 10) where |B| is the length of B.
+
+Output
+Print the answer required above.
+
+Example
+InputCopy
+abcd
+ef
+OutputCopy
+4 2
+abcdef
+ebcd af */
+   
+      string A, B;
+      cin >> A >> B;
+      cout << A.size() << " " << B.size() << endl;
+      cout << A + B << endl;
+      cout << B[0] + A.substr(1) << " " << A[0] + B.substr(1) << endl;
+      return 0;
+
 }
