0b2a67a64cbafe5e2a38739838525a1a227065d1
Fajla-Rabby <fajla.rabby008@gmail.com>
2024-10-19 06:04:25 +0600

    Stop tracking ignored files

--- test2.cpp
+++ test2.cpp
@@ -7,18 +7,24 @@ int main()
 {
     int tt;
     cin >> tt;
-    int result = 0;
+    // int result = 0;
+     int x = 0;
 
     while (tt--)
     {
-        int ar[3];
-        cin>>ar[0] >> ar[1] >> ar[2];
-        if (ar[0] + ar[1] + ar[2] >= 2)
-        {
-            result += 1;
-        }
-    }
-    cout << result;
+        int n, k;
+        cin>>n>>k;
+        int result = 0;;
 
+        while(n>0)
+        {
+            result+= n % k;
+            n = (n - pow(k,x));
+            x++;
+            cout<<x;
+        }
+        
+    }
+   
     return 0;
 }
