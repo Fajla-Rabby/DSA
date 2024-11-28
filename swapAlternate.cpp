aac7c406474b0a35b4f8684a4c2c0760da138d9a
Fajla-Rabby <fajla.rabby008@gmail.com>
2024-11-28 23:33:29 +0600

    swap alternate revised also swap without temp variable

--- swapAlternate.cpp
+++ swapAlternate.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void printArray(int arr[], int n)
+void swapAlternate(int arr[], int size)
 {
-    for(int i = 0; i< n; i++)
+    for(int i = 0; i <size; i+=2)
+    {
+        if(i + 1 < size)
+        {
+            arr[i] = arr[i] + arr[i + 1];
+            arr[i + 1] = arr[i] - arr[i + 1];
+            arr[i] = arr[i] - arr[i + 1];
+
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
     {
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
 
-void swapAlternate(int arr[], int size)
-{
-    for (int i = 0; i < size; i += 2)
-    {
-        if (i + 1 < size)
-        {
-            swap(arr[i], arr[i + 1]);
-        }
-    }
-}
-
 int main()
 {
-    int even[8] = {5, 2, 9, 4, 7, 6, 1, 0};
-    int odd[5] = {11, 33, 9, 76, 43};
+    int even[6] = {3,5,2,5,1,4};
+    int odd[5] = {3,5,2,5,1};
 
-    swapAlternate(even, 8);
-    printArray(even, 8);
-
+    swapAlternate(even, 6);
+    printArray(even, 6);
     cout<<endl;
-
     swapAlternate(odd, 5);
     printArray(odd, 5);
     return 0;
